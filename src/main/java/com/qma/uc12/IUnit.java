ea585fb428e923e0020ca116447adbe65bc427dc
kirankurhade-hub <kiran.kurhade@bridgelabz.com>
2026-05-12 20:03:40 +0530

    feat: UC12-SubtractionAndDivision - add implementation

---
+++ src/main/java/com/qma/uc12/IUnit.java
@@ -0,0 +1,2 @@
+package com.qma.uc12;
+public interface IUnit { double getBaseUnitFactor(); String getSymbol(); String getCategory(); }
---
+++ src/main/java/com/qma/uc12/WeightUnit.java
@@ -0,0 +1,9 @@
+package com.qma.uc12;
+public enum WeightUnit implements IUnit {
+    GRAM(1.0,"g"),KILOGRAM(1000.0,"kg");
+    private final double f;private final String s;
+    WeightUnit(double f,String s){this.f=f;this.s=s;}
+    @Override public double getBaseUnitFactor(){return f;}
+    @Override public String getSymbol(){return s;}
+    @Override public String getCategory(){return "WEIGHT";}
+}
